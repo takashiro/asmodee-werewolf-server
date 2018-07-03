d668e0c1e6347437b4a90e308d21e3f9466234a8
Takashiro <takashiro@qq.com>
2018-07-03 20:52:19 +0800

    Add service config and remove service installer

---
+++ service-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SERVICE_USER=karuta
+SERVICE_NAME=asmodee-werewolf
+SERVICE_DESC="Werewolves of Miller's Hollow"
--- sh/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SERVICE=asmodee-werewolf
-
-service $SERVICE stop
-
-rm -f /etc/init.d/$SERVICE
-update-rc.d $SERVICE remove
