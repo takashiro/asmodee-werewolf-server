6bf49ad05808583203194a86b1394ddaa8e16b4b
Kazuichi Takashiro <takashiro@qq.com>
2019-04-13 21:58:28 +0800

    Always use tabs

--- game/Engine.js
+++ game/Engine.js
@@ -8,12 +8,12 @@ const SkillList = require('../skills');
  * @param {Array} a
  */
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
+	for (let i = a.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
 }
 
 class Engine {
