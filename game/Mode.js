b009ac623d504f999131fbec337fc0b81389e95e
Kazuichi Takashiro <takashiro@qq.com>
2020-08-15 17:12:26 +0800

    Add @asmodee/werewolf-core

--- game/Mode.js
+++
@@ -1,7 +0,0 @@
-
-const Mode = {
-	Normal: 1,
-	Dual: 2,
-};
-
-module.exports = Mode;
--- game/Team.js
+++
@@ -1,9 +0,0 @@
-
-const Team = {
-	Unknown: 0,
-	Werewolf: 1,
-	Villager: 2,
-	Other: 3,
-};
-
-module.exports = Team;
