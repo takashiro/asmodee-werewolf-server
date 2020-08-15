85a3dc4d2f4b7bf92dd9c97dad0028f43100016e
Kazuichi Takashiro <takashiro@qq.com>
2020-08-15 16:38:36 +0800

    Immigrate to TypeScript

--- game/Timing.js
+++
@@ -1,8 +0,0 @@
-
-const Timing = {
-	ShuffleCards: 0x1,
-	DrawCard: 0x2,
-	TakeSeat: 0x3,
-};
-
-module.exports = Timing;
---
+++ src/game/GameEvent.ts
@@ -0,0 +1,8 @@
+
+enum GameEvent {
+	ShuffleCards,
+	DrawCard,
+	TakeSeat,
+}
+
+export default GameEvent;
