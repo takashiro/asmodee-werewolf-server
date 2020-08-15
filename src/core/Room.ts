fd266194a9c464136d772be7a27e247731b7e924
Kazuichi Takashiro <takashiro@qq.com>
2020-08-15 17:52:14 +0800

    Rewrite Room

---
+++ src/core/Room.ts
@@ -0,0 +1,50 @@
+import { RoomConfig } from '@asmodee/werewolf-core';
+
+import GameDriver from '../game/GameDriver';
+import randstr from '../util/randstr';
+
+export default class Room {
+	protected id: number;
+
+	protected salt: string;
+
+	protected ownerKey: string;
+
+	protected driver: GameDriver;
+
+	protected timer?: NodeJS.Timer;
+
+	constructor() {
+		this.id = 0;
+		this.salt = randstr(8);
+		this.ownerKey = randstr(32);
+		this.driver = new GameDriver;
+	}
+
+	getId(): number {
+		return this.id;
+	}
+
+	getDriver(): GameDriver {
+		return this.driver;
+	}
+
+	toJSON(): RoomConfig {
+		return {
+			id: this.id,
+			salt: this.salt,
+			...this.driver.toJSON(),
+		};
+	}
+
+	setTimer(timer: NodeJS.Timer): void {
+		this.timer = timer;
+	}
+
+	destroy(): void {
+		if (this.timer) {
+			clearTimeout(this.timer);
+			delete this.timer;
+		}
+	}
+}
---
+++ src/util/randstr.ts
@@ -0,0 +1,22 @@
+/**
+ * Generate a random string
+ * @param length
+ * @return
+ */
+export default function randstr(length: number): string {
+	let str = '';
+	for(let i = 0; i < length; i++){
+		let rand = Math.floor(Math.random() * 62);
+		if(rand >= 0 && rand <= 25){
+			rand += 0x41;
+		}else if(rand <= 51){
+			rand -= 26;
+			rand += 0x61;
+		}else{
+			rand -= 52;
+			rand += 0x30;
+		}
+		str += String.fromCharCode(rand);
+	}
+	return str;
+}
