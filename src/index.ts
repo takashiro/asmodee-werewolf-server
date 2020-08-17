e6ed4eb3c1a931b86a2aac0a0287a7ddbce0afa9
Kazuichi Takashiro <takashiro@qq.com>
2020-08-17 21:02:53 +0800

    Launch application

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import config from './util/config';
+import { lobby } from './core';
+import app from './app';
+
+(async function main(): Promise<void> {
+	await config.read();
+	lobby.setCapacity(config.lobbyCapacity);
+	app.listen(config.socket);
+}());
---
+++ src/util/config.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+
+const { readFile } = fs.promises;
+
+export class Config {
+	socket: number | string;
+
+	lobbyCapacity: number;
+
+	constructor() {
+		this.socket = '/var/run/asmodee-werewolf.sock';
+		this.lobbyCapacity = 1000;
+	}
+
+	async read(file = 'config.json'): Promise<void> {
+		if (!fs.existsSync(file)) {
+			return;
+		}
+
+		try {
+			const content = await readFile(file, 'utf-8');
+			const config = JSON.parse(content);
+			Object.assign(this, config);
+		} catch (error) {
+			// Ignore
+		}
+	}
+}
+
+const config = new Config();
+export default config;
